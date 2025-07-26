fd5d1ac607b54be18cc15580c21e1c40435f6404
Nícollas <nicollaascarvalho@gmail.com>
2025-07-26 20:51:39 -0300

    Create Hero.tsx

---
+++ app/app/components/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">Olá, eu sou o Nicz 👋</h1>
+        <p className="mt-4 text-lg text-gray-400">Desenvolvedor Front-end</p>
+      </div>
+    </section>
+  );
+}
